83e384ae22e1dc8aa80fddd5552d07186ddc46d7
TRogalski <tomracc@gmail.com>
2019-06-09 20:40:50 +0200

    SpringBoot project initial setup

---
+++ src/main/java/com/healthme/HealthmeApplication.java
@@ -0,0 +1,13 @@
+package com.healthme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthmeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/healthme/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.healthme.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String getHomeView() {
+        return "Hello Mr Lombok!";
+    }
+}
