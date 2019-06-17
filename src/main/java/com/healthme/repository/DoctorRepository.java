e20ecc6e6ca9293b54c198a3da0e1bd6fbf18bee
dawidkaszuba <d.kaszuba89@gmail.com>
2019-06-17 22:30:21 +0200

    AddedAdd_Doctor_Repository

---
+++ src/main/java/com/healthme/repository/DoctorRepository.java
@@ -0,0 +1,9 @@
+package com.healthme.repository;
+
+import com.healthme.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
