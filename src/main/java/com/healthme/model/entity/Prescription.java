10444ac3f16074067cffc9061ec4de9b8db2481e
tomaszbabinski <tommybab@gmail.com>
2019-07-13 18:30:49 +0200

    Prescription entity added

---
+++ src/main/java/com/healthme/model/entity/Prescription.java
@@ -0,0 +1,76 @@
+package com.healthme.model.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "prescriptions")
+public class Prescription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    @ManyToOne
+    private Patient patient;
+
+    private LocalDateTime created;
+
+    private LocalDateTime expiredDate;
+
+    private String prescribedItems;
+
+    public Prescription() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getExpiredDate() {
+        return expiredDate;
+    }
+
+    public void setExpiredDate(LocalDateTime expiredDate) {
+        this.expiredDate = expiredDate;
+    }
+
+    public String getPrescribedItems() {
+        return prescribedItems;
+    }
+
+    public void setPrescribedItems(String prescribedItems) {
+        this.prescribedItems = prescribedItems;
+    }
+}
