5a72d6bf98d4e0854861ad20253490dae705305f
dawidkaszuba <d.kaszuba89@gmail.com>
2019-07-14 14:20:06 +0200

    added visit entity

--- src/main/java/com/healthme/model/entity/SickNote.java
+++ src/main/java/com/healthme/model/entity/SickNote.java
@@ -44,4 +44,103 @@ public class SickNote {
 
     @Pattern(regexp = "[ABCDE]{1,4}")
     private String codes;
+
+    public SickNote() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public int getInsuredIn() {
+        return insuredIn;
+    }
+
+    public void setInsuredIn(int insuredIn) {
+        this.insuredIn = insuredIn;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getStatisticalNumberOfDisease() {
+        return statisticalNumberOfDisease;
+    }
+
+    public void setStatisticalNumberOfDisease(int statisticalNumberOfDisease) {
+        this.statisticalNumberOfDisease = statisticalNumberOfDisease;
+    }
+
+    public LocalDate getInabilityToWorkFrom() {
+        return inabilityToWorkFrom;
+    }
+
+    public void setInabilityToWorkFrom(LocalDate inabilityToWorkFrom) {
+        this.inabilityToWorkFrom = inabilityToWorkFrom;
+    }
+
+    public LocalDate getInabilityToWorkTo() {
+        return inabilityToWorkTo;
+    }
+
+    public void setInabilityToWorkTo(LocalDate inabilityToWorkTo) {
+        this.inabilityToWorkTo = inabilityToWorkTo;
+    }
+
+    public int getDaysInHospital() {
+        return daysInHospital;
+    }
+
+    public void setDaysInHospital(int daysInHospital) {
+        this.daysInHospital = daysInHospital;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public int getKinshipCode() {
+        return kinshipCode;
+    }
+
+    public void setKinshipCode(int kinshipCode) {
+        this.kinshipCode = kinshipCode;
+    }
+
+    public int getMedicalIndications() {
+        return medicalIndications;
+    }
+
+    public void setMedicalIndications(int medicalIndications) {
+        this.medicalIndications = medicalIndications;
+    }
+
+    public String getCodes() {
+        return codes;
+    }
+
+    public void setCodes(String codes) {
+        this.codes = codes;
+    }
 }
--- src/main/java/com/healthme/repository/VisitRepository.java
+++ src/main/java/com/healthme/repository/VisitRepository.java
@@ -1,4 +1,9 @@
 package com.healthme.repository;
 
-public class VisitRepository {
+import com.healthme.model.entity.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitRepository extends JpaRepository<Visit, Long> {
 }
