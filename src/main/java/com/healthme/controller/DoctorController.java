6e7367e77fd1e7cbf6fba51a24d897aa599ac5fd
dawidkaszuba <d.kaszuba89@gmail.com>
2019-07-17 20:18:55 +0200

    added visits view

--- src/main/java/com/healthme/controller/DoctorController.java
+++ src/main/java/com/healthme/controller/DoctorController.java
@@ -1,15 +1,38 @@
 package com.healthme.controller;
 
+import com.healthme.model.entity.Doctor;
+import com.healthme.repository.VisitRepository;
+import com.healthme.service.doctor.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/doctor")
 public class DoctorController {
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String getHomeView(){
-        return "doctor/home";
+
+    private final DoctorService doctorService;
+
+    @Autowired
+    public DoctorController(DoctorService doctorService, VisitRepository visitRepository) {
+        this.doctorService = doctorService;
     }
+
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String getHomeView(Model model, Principal principal){
+        Doctor doctor = doctorService.findDoctorByEmail(principal.getName());
+        model.addAttribute("visits",
+            doctorService.findAllVisitByDoctorIdForCurrentDay(doctor.getId()));
+    return "doctor/home";
+    }
+
+
+
+
 }
--- src/main/java/com/healthme/repository/VisitRepository.java
+++ src/main/java/com/healthme/repository/VisitRepository.java
@@ -2,8 +2,15 @@ package com.healthme.repository;
 
 import com.healthme.model.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
+    @Query(value = "SELECT * from visit where local_date_time LIKE :nowOnlyDate% AND doctor_id=:doctorId", nativeQuery = true)
+    List<Visit> findAllVisitByDoctorIdForCurrentDay(@Param("nowOnlyDate") String nowOnlyDate, @Param("doctorId") Long doctorId);
+
 }
